ec5bcb6fbf43ca73fac904d621c2fdda2bd78ffd
Nikki-Resendizx <nikrezx5@gmail.com>
2026-09-08 14:27:01 -0600

    Add constantes.js with configuration constants

---
+++ config/constantes.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ADMIN_ID: 8695673050,
+  CATS_USUARIO: ['CANALES ADULTOS','GRUPOS ADULTOS','VENTAS','PUBLICITARIOS','ENTRETENIMIENTO','ARTE'],
+  CAT_BOTS: 'BOTS',
+  PALABRAS_PROHIBIDAS: ['cp','child','niños','niñas','menor','gore','zoofilia','necrophilia','baby','kid'],
+  VERSION: 'V9 MODULAR'
+};
